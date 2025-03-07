05757149ac865999d95428b150943247f088c5ab
karolyangqian <karolyangqian14@gmail.com>
2025-03-08 01:06:27 +0700

    feat: Quack targeted scan

---
+++ src/alternative-bots/Quack/Quack.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Drawing;
+using Robocode.TankRoyale.BotApi;
+using Robocode.TankRoyale.BotApi.Events;
+
+// ------------------------------------------------------------------
+// Quack
+// ------------------------------------------------------------------
+// A sample bot original made for Robocode by Mathew Nelson.
+// Ported to Robocode Tank Royale by Flemming N. Larsen.
+//
+// Sits still. Tracks and fires at the nearest bot it sees.
+// ------------------------------------------------------------------
+public class Quack : Bot
+{
+    private double scannedEnemyX;
+    private double scannedEnemyY;
+    private bool anEnemyScanned;
+
+
+    // The main method starts our bot
+    static void Main(string[] args)
+    {
+        new Quack().Start();
+    }
+
+    // Constructor, which loads the bot config file
+    Quack() : base(BotInfo.FromFile("Quack.json")) { }
+
+    // Called when a new round is started -> initialize and do some movement
+    public override void Run()
+    {
+        // Set colors
+        var pink = Color.FromArgb(0xFF, 0x69, 0xB4);
+        BodyColor = pink;
+        TurretColor = pink;
+        RadarColor = pink;
+        ScanColor = pink;
+        BulletColor = pink;
+        AdjustGunForBodyTurn = true;
+        // AdjustRadarForGunTurn = true;
+        AdjustRadarForBodyTurn = true;
+        anEnemyScanned = false;
+
+        // Loop while running
+        while (IsRunning)
+        {
+            // TurnGunLeft(10); // Scans automatically as radar is mounted on gun
+            if (anEnemyScanned) {
+                // TrackScanAt(scannedEnemyX, scannedEnemyY);
+                TrackScanWithGunAt(scannedEnemyX, scannedEnemyY);
+                // shootAt(scannedEnemyX, scannedEnemyY, 1, 0);
+            } else {
+                TurnGunLeft(20);
+            }
+            // anEnemyScanned = false;
+            // Forward(20);
+            // TurnLeft(50);
+            // trackScanAt(900, 900);
+            // Back(20);
+        }
+    }
+
+    // We scanned another bot -> we have a target, so go get it
+    // public override void OnScannedBot(ScannedBotEvent e)
+    // {
+    //     // Calculate direction of the scanned bot and bearing to it for the gun
+    //     var bearingFromGun = GunBearingTo(e.X, e.Y);
+
+    //     // Turn the gun toward the scanned bot
+    //     TurnGunLeft(bearingFromGun);
+
+    //     // If it is close enough, fire!
+    //     if (Math.Abs(bearingFromGun) <= 3 && GunHeat == 0)
+    //         Fire(Math.Min(3 - Math.Abs(bearingFromGun), Energy - .1));
+
+    //     // Generates another scan event if we see a bot.
+    //     // We only need to call this if the gun (and therefore radar)
+    //     // are not turning. Otherwise, scan is called automatically.
+    //     if (bearingFromGun == 0)
+    //         Rescan();
+    // }
+
+    public override void OnScannedBot(ScannedBotEvent e)
+    {
+        scannedEnemyX = e.X;
+        scannedEnemyY = e.Y;
+        anEnemyScanned = true;
+        Fire(1);
+        // shootAt(scannedEnemyX, scannedEnemyY, 1, 5);
+        // AimGunAt(scannedEnemyX, scannedEnemyY, 0);
+    }
+
+    // We won the round -> do a victory dance!
+    public override void OnWonRound(WonRoundEvent e)
+    {
+        // Victory dance turning right 360 degrees 100 times
+        TurnLeft(36_000);
+    }
+
+    // normalizes a bearing to between +180 and -180
+    private double NormalizeBearing(double angle) {
+        while (angle >  180) angle -= 360;
+        while (angle < -180) angle += 360;
+        return angle;
+    }
+
+    private void TrackScanAt(double x, double y) {
+        var bearingFromRadar = RadarBearingTo(x, y);
+        TurnRadarLeft(bearingFromRadar + 10);
+        bearingFromRadar = RadarBearingTo(x, y);
+        TurnRadarLeft(bearingFromRadar - 10);
+    }
+
+    private void TrackScanWithGunAt(double x, double y) {
+        AdjustRadarForGunTurn = false;
+        var bearingFromGun = GunBearingTo(x, y);
+        TurnGunLeft(bearingFromGun + 10);
+        bearingFromGun = RadarBearingTo(x, y);
+        TurnGunLeft(bearingFromGun - 10);
+    }
+
+    private void AimGunAt(double x, double y, double degreesMaxOffset) {
+        var bearingFromGun = GunBearingTo(X, y);
+        var rnd = new Random();
+        TurnGunLeft(bearingFromGun + GetRandomSign()*rnd.NextDouble()*degreesMaxOffset);
+    }
+
+    private void ShootAt(double x, double y, double power, double degreesMaxOffset) {
+        AimGunAt(X, y, degreesMaxOffset);
+        Fire(power);
+    }
+
+    private double DegreesToRadians(double degrees) {
+        return degrees * (Math.PI / 180);
+    } 
+
+    private int GetRandomSign() {
+        var rnd = new Random();
+        return rnd.Next(0, 2) == 0 ? -1 : 1;
+    }
+}
