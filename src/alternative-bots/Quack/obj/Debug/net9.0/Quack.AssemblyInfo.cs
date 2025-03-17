eaee1b1c743e2c14eb40518eb6b5d8f89beb1faa
karolyangqian <karolyangqian14@gmail.com>
2025-03-17 20:39:30 +0700

    feat: ignore dumps

--- src/alternative-bots/Quack/obj/Debug/net9.0/Quack.AssemblyInfo.cs
+++ src/alternative-bots/Quack/obj/Debug/net9.0/Quack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f45e04f0da8cdba524a137b28d780a6e7e00c51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94bdd3f299757d97b6e8a6a19cd8f8bd6f1881ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
