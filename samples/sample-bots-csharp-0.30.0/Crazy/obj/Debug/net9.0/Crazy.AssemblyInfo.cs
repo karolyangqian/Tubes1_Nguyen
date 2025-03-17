27ded5b6e6c6f88db6429886eaf026b8a5ca12c9
karolyangqian <karolyangqian14@gmail.com>
2025-03-17 20:15:25 +0700

    fix: smooth movement

--- samples/sample-bots-csharp-0.30.0/Crazy/obj/Debug/net9.0/Crazy.AssemblyInfo.cs
+++ samples/sample-bots-csharp-0.30.0/Crazy/obj/Debug/net9.0/Crazy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crazy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05757149ac865999d95428b150943247f088c5ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f45e04f0da8cdba524a137b28d780a6e7e00c51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Crazy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crazy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- samples/sample-bots-csharp-0.30.0/MyFirstDroid/obj/Debug/net9.0/MyFirstDroid.AssemblyInfo.cs
+++ samples/sample-bots-csharp-0.30.0/MyFirstDroid/obj/Debug/net9.0/MyFirstDroid.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstDroid")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05757149ac865999d95428b150943247f088c5ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f45e04f0da8cdba524a137b28d780a6e7e00c51")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstDroid")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstDroid")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
